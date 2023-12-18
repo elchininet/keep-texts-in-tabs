fe12de68851746600f113964b05e5616fa5b83f4
Iván Pereira <elchininet@gmail.com>
2023-12-18 02:59:09 +0100

    Build and upload coverage (#18)

--- rollup.config.js
+++ rollup.config.js
@@ -2,29 +2,48 @@ import ts from 'rollup-plugin-ts';
 import json from '@rollup/plugin-json';
 import { terser } from 'rollup-plugin-terser';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import istanbul from 'rollup-plugin-istanbul';
 
-export default {
-    plugins: [
-        nodeResolve(),
-        json(),
-        ts({
-            browserslist: false
-        }),
-        terser({
-            output: {
-                comments: false
-            }
-        })
-    ],
-    input: 'src/keep-texts-in-tabs.ts',
-    output: [
-        {
+export default [
+    {
+        plugins: [
+            nodeResolve(),
+            json(),
+            ts({
+                browserslist: false
+            }),
+            terser({
+                output: {
+                    comments: false
+                }
+            })
+        ],
+        input: 'src/keep-texts-in-tabs.ts',
+        output: {
             file: 'dist/keep-texts-in-tabs.js',
             format: 'iife'
-        },
-        {
+        }
+    },
+    {
+        plugins: [
+            nodeResolve(),
+            json({
+                preferConst: true
+            }),
+            ts({
+                browserslist: false
+            }),
+            istanbul({
+                exclude: [
+                    'node_modules/**/*',
+                    'package.json'
+                ]
+            }),
+        ],
+        input: 'src/keep-texts-in-tabs.ts',
+        output: {
             file: '.hass/config/www/keep-texts-in-tabs.js',
             format: 'iife'
         }
-    ]
-};
+    }
+];
