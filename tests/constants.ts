ab8fb0e6d3c8245c6471ca4e29ed4377ffb4b004
Iván Pereira Muñoz <elchininet@gmail.com>
2025-05-04 01:00:21 +0200

    Fix tests

--- tests/constants.ts
+++ tests/constants.ts
@@ -1,3 +1,3 @@
 export const BASE_PAGE = 'http://host.docker.internal:8123';
-export const HEADER_SELECTOR = '.header ha-tabs paper-tab:last-of-type';
-export const TABS_CONTENT_SELECTOR = '#tabsContent';
+export const HEADER_SELECTOR = '.header sl-tab-group sl-tab:last-of-type';
+export const TABS_CONTENT_SELECTOR = '.tab-group__tabs';
