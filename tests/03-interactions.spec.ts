12cf4a99adb28236d17276bbd516f82174875d61
Iván Pereira Muñoz <elchininet@gmail.com>
2025-09-26 17:43:11 +0300

    Prepare the plugin for Home Assistant 2025.10.1

--- tests/03-interactions.spec.ts
+++ tests/03-interactions.spec.ts
@@ -6,7 +6,6 @@ import {
 } from './constants';
 
 const BUTTON_MENU = '.action-items > ha-button-menu:last-of-type';
-const EXIT_EDIT_MODE_OLD = 'mwc-button.exit-edit-mode'; // Home Assistant < 2025.8.1
 const EXIT_EDIT_MODE = 'ha-button.exit-edit-mode';
 const DIALOG_HEADER = 'ha-dialog-header';
 
@@ -19,10 +18,7 @@ test.beforeEach(async ({ page }) => {
     await expect(page.locator(HEADER_SELECTOR)).toBeVisible();
     await page.locator(BUTTON_MENU).click();
     await page.getByRole('menuitem', { name: 'Edit dashboard' }).click();
-    await expect(
-        // Remove EXIT_EDIT_MODE_OLD after Home Assistant 2025.8.1
-        page.locator(EXIT_EDIT_MODE_OLD).or(page.locator(EXIT_EDIT_MODE))
-    ).toBeVisible();
+    await expect(page.locator(EXIT_EDIT_MODE)).toBeVisible();
 
 });
 
@@ -33,13 +29,9 @@ test('Edit a storage mode dashboard', async ({ page }) => {
     await page.getByRole('menuitem', { name: 'Raw configuration editor' }).click();
     await expect(page.locator('hui-editor')).toBeVisible();
     await page.locator('ha-top-app-bar-fixed > ha-icon-button').click();
-    await expect(
-        // Remove EXIT_EDIT_MODE_OLD after Home Assistant 2025.8.1
-        page.locator(EXIT_EDIT_MODE_OLD).or(page.locator(EXIT_EDIT_MODE))
-    ).toBeVisible();
+    await expect(page.locator(EXIT_EDIT_MODE)).toBeVisible();
     await expect(page.locator(TABS_CONTENT_SELECTOR)).toHaveScreenshot('01-storage-mode-edit-view.png');
-    // Remove EXIT_EDIT_MODE_OLD after Home Assistant 2025.8.1
-    await page.locator(EXIT_EDIT_MODE_OLD).or(page.locator(EXIT_EDIT_MODE)).click();
+    await page.locator(EXIT_EDIT_MODE).click();
 
 });
 
@@ -49,8 +41,7 @@ test('Moving a view', async ({ page }) => {
     await expect(page.locator(TABS_CONTENT_SELECTOR)).toHaveScreenshot('02-storage-mode-moving-view-right.png');
     await page.locator('ha-icon-button-arrow-prev:visible').click();
     await expect(page.locator(TABS_CONTENT_SELECTOR)).toHaveScreenshot('01-storage-mode-edit-view.png');
-    // Remove EXIT_EDIT_MODE_OLD after Home Assistant 2025.8.1
-    await page.locator(EXIT_EDIT_MODE_OLD).or(page.locator(EXIT_EDIT_MODE)).click();
+    await page.locator(EXIT_EDIT_MODE).click();
 
 });
 
@@ -64,8 +55,7 @@ test('Adding a new view', async ({ page }) => {
     const comboBoxItem = page.locator('vaadin-combo-box-item');
     const saveButton = page.locator('ha-dialog > ha-button');
     const tabsContainer = page.locator(TABS_CONTENT_SELECTOR);
-    // Remove EXIT_EDIT_MODE_OLD after Home Assistant 2025.8.1
-    const exitEditMode = page.locator(EXIT_EDIT_MODE_OLD).or(page.locator(EXIT_EDIT_MODE));
+    const exitEditMode = page.locator(EXIT_EDIT_MODE);
     const headerTabs = page.locator(HEADER_SELECTOR);
 
     const buttonMenu = page.locator(BUTTON_MENU);
